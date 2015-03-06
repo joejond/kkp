f9a3ad20d84a8b549dff1cf8339632ab21ddbc77
jono <jono.daunbiru@gmail.com>
2015-03-06 15:39:30 +0700

    pertama kali

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('kkp.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'kkp'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
